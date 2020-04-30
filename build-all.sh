66db1c687541cf9d693783540a164e37d5be3d3b
Severin Gehwolf <sgehwolf@redhat.com>
2020-04-30 18:15:19 +0200

    Add support to download a build JDK 11
    If none is being given, then an OpenJDK Project Build is
    being used.

--- build-all.sh
+++ build-all.sh
@@ -38,7 +38,17 @@ buildSubstrate() {
   popd > /dev/null
 }
 
-sanityCheck() {
+downloadJDKandSanityCheck() {
+  if [ "${BUILDER_JDK_IMAGE}_" == "_" ]; then
+    echo "No builder JDK provided. Downloading..."
+    TEMP_DIR="$(mktemp -d)"
+    pushd $TEMP_DIR
+      wget -O jdk-11.0.8-ea.tar.gz https://api.adoptopenjdk.net/v3/binary/latest/11/ea/linux/x64/jdk/hotspot/normal/openjdk
+      wget -O static-libs-11.0.8-ea.tar.gz https://api.adoptopenjdk.net/v3/binary/latest/11/ea/linux/x64/staticlibs/hotspot/normal/openjdk
+    popd
+    BUILDER_JDK_IMAGE="$TEMP_DIR/jdk-11.0.8-ea.tar.gz"
+    BUILDER_JDK_STATIC_LIBS_IMAGE="$TEMP_DIR/static-libs-11.0.8-ea.tar.gz"
+  fi
   if [ ! -e "$BUILDER_JDK_IMAGE" ]; then
     echo "Error: Builder JDK tarball not found: $BUILDER_JDK_IMAGE"
     exit 1
@@ -49,7 +59,7 @@ sanityCheck() {
   fi
 }
 
-sanityCheck
+downloadJDKandSanityCheck
 RESULTS_BASE_DIR="$(pwd)/results"
 rm -rf "$RESULTS_BASE_DIR"
 
@@ -59,7 +69,7 @@ pushd $(pwd)/graal-builder > /dev/null
   tar -xf $BUILDER_JDK_IMAGE
   tar -xf $BUILDER_JDK_STATIC_LIBS_IMAGE
   OPENJDK_GRAAL_BUILDER="$(pwd)/$(ls -d *)"
-  echo "JDK 11.0.8 graal builder image in: $OPENJDK_GRAAL_BUILDER"
+  echo "JDK 11 graal builder image in: $OPENJDK_GRAAL_BUILDER"
 popd > /dev/null
 export OPENJDK_GRAAL_BUILDER
 export RESULT_DIR="$RESULTS_BASE_DIR/substrate"
@@ -73,3 +83,8 @@ buildSubstrate "$(pwd)/graal-ce/linux/x86_64/build-svm.sh"
 
 echo "Results in: "
 find $RESULTS_BASE_DIR -name \*.tar.gz
+
+# Clean up potentially downloaded build JDK
+if [ -d $TEMP_DIR ]; then
+  rm -rf $TEMP_DIR
+fi
